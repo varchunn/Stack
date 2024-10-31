2adfab7c56f644a8da6d40e06b49b2cf68d1a9d1
varchunn <varvaram186@gmail.com>
2024-10-31 09:46:49 +0200

    Add files via upload

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,14 +1,84 @@
-//
-//  main.cpp
-//  Stack
-//
-//  Created by Варя Мінєнкова on 31.10.2024.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+template<typename Essence>
+class Stack
+{
+private:
+    struct NodeOfDestiny 
+    {
+        Essence payload;
+        NodeOfDestiny* nextPhase;
+
+        NodeOfDestiny(const Essence& element) : payload(element), nextPhase(nullptr) {}
+    };
+
+    NodeOfDestiny* apex;
+    int magnitude;
+public:
+    
+   Stack() : apex(nullptr), magnitude(0) {}
+    ~Stack()
+    {
+        while (!isEjected()) 
+        {
+            pullTime();
+        }
+    }
+
+    void pushTime(const Essence& element) 
+    {
+        NodeOfDestiny* newPhase = new NodeOfDestiny(element);
+        newPhase->nextPhase = apex;
+        apex = newPhase;
+        magnitude++;
+    }
+    
+    void pullTime() 
+    {
+        if (isEjected()) 
+        {
+            std::cerr << "Stack is empty. Cannot pull.\n";
+            return;
+        }
+        NodeOfDestiny* oldPhase = apex;
+        apex = apex->nextPhase;
+        delete oldPhase;
+        magnitude--;
+    }
+    Essence peakTime() const 
+    {
+        if (isEjected()) 
+        {
+            std::cerr << "Stack is empty. Cannot peak.\n";
+            return Essence();
+        }
+        return apex->payload;
+    }
+
+    bool isEjected() const
+    {
+        return apex == nullptr;
+    }
+
+    int sizeOfFlux() const 
+    {
+        return magnitude;
+    }
+};
+
+int main() 
+{
+    Stack<int> myFlux;
+
+    myFlux.pushTime(1);
+    myFlux.pushTime(2);
+    myFlux.pushTime(3);
+
+    std::cout << "Peak: " << myFlux.peakTime() << std::endl;
+    myFlux.pullTime();
+    std::cout << "Peak after pull: " << myFlux.peakTime() << std::endl;
+
+    std::cout << "Size: " << myFlux.sizeOfFlux() << std::endl;
     return 0;
 }
+
