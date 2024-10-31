64bafb94c9c77e6c08672f7235d3ea1389ee6686
varchunn <varvaram186@gmail.com>
2024-10-31 09:41:36 +0200

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Варя Мінєнкова on 31.10.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
